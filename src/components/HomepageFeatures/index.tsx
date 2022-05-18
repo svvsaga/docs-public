7bca88893ff4931c1adc2c1f4dd4bacc4188ad28
Preben Aas <prebenaas@gmail.com>
2022-05-18 13:02:30 +0200

    Implemented new design for landing page

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,72 +1,18 @@
 import React from 'react'
-import clsx from 'clsx'
 import Link from '@docusaurus/Link'
 import styles from './styles.module.css'
-
-type FeatureItem = {
-  title: string
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>
-  description: JSX.Element
-}
-
-const FeatureList: FeatureItem[] = [
-  {
-    title: 'Enkelt å bruke',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Saga senker terskelen for å komme i gang med dataanalyse på tvers av
-        SVVs mange ulike datakilder.
-      </>
-    ),
-  },
-  {
-    title: 'Gjør datadrevne valg',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: <>Når du vet mer, kan du treffe bedre beslutninger!</>,
-  },
-  {
-    title: 'Alt på ett sted',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Med populære og standardiserte datasett på samme sted, kan man enklere
-        se sammenhenger.
-      </>
-    ),
-  },
-]
-
-function Feature({ title, Svg, description }: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  )
-}
 
 export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-        <div className="row">
-          <div
-            className={clsx(
-              'col text--center margin-top--lg',
-              styles.gettingStarted
-            )}>
-            <Link to="/google-cloud-platform/kom-i-gang">Kom i gang!</Link>
+        <div className="landing-page">
+          <h1 className="hero__title">Saga dokumentasjon</h1>
+          <p className="hero__subtitle">Vi senker terskelen for å komme i gang med dataanalyse på tvers av vegvesenets mange ulike datakilder.</p>
+          <div className={styles.buttons}>
+            <Link className="button button--secondary button--lg" to="/intro">
+              Les dokumentasjonen
+            </Link>
           </div>
         </div>
       </div>
