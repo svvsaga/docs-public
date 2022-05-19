411d003908d718f78aca077a18bacd1cc27808ae
Preben Aas <prebenaas@gmail.com>
2022-05-19 09:26:31 +0200

    Port animation to react

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Layout from '@theme/Layout'
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import HomepageFeatures from '@site/src/components/HomepageFeatures'
 
 export default function Home(): JSX.Element {
   return (
-    <Layout
-      title="Dokumentasjon"
-      description="">
-        <HomepageFeatures />
-    </Layout>
+    <>
+      <Layout
+        title="Dokumentasjon"
+        description="">
+          <HomepageFeatures />
+      </Layout>
+    </>
   )
 }
